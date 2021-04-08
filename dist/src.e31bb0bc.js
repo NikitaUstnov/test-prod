edfbeff98399c9bb4b5bd0d6968c188f4981198d
Nikita Ustinov <65974693+NikitaUstnov@users.noreply.github.com>
2021-04-08 23:36:09 +0300

    mobile-done

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -189,10 +189,25 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
+},{"C:\\Users\\Nikita\\Documents\\GitHub\\test-prod\\src\\images\\bacground-mobile.png":[["bacground-mobile.3892353c.png","images/bacground-mobile.png"],"images/bacground-mobile.png"],"C:\\Users\\Nikita\\Documents\\GitHub\\test-prod\\src\\images\\bacground-desctop.jpg":[["bacground-desctop.15fa34be.jpg","images/bacground-desctop.jpg"],"images/bacground-desctop.jpg"],"_css_loader":"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
 "use strict";
 
 require("./main.less");
+
+var menu = document.querySelector('.header__nav--wrapper');
+var menuBtn = document.querySelector('.header__open-menu');
+var menuBtnClose = document.querySelector('.header__close-menu');
+menuBtn.addEventListener('click', open);
+
+function open() {
+  menu.classList.add('is-visible');
+}
+
+menuBtnClose.addEventListener('click', close);
+
+function close() {
+  menu.classList.remove('is-visible');
+}
 },{"./main.less":"main.less"}],"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -221,7 +236,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49905" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49868" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-import './main.less'
+import './main.less';
+const menu = document.querySelector('.header__nav--wrapper');
+const menuBtn = document.querySelector('.header__open-menu');
+const menuBtnClose = document.querySelector('.header__close-menu');
+
+menuBtn.addEventListener('click', open);
+
+function open() {
+  menu.classList.add('is-visible');
+}
+
+menuBtnClose.addEventListener('click', close);
+function close() {
+  menu.classList.remove('is-visible');
+}
